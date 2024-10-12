b2fcdb25ed433a6a522d9aaf4d841c5742638769
Tomrm96 <tomrutter96@hotmail.com>
2024-10-12 13:59:52 +0100

    Completed Game

--- Pong/ball.py
+++ Pong/ball.py
@@ -1,21 +1,42 @@
 from turtle import Turtle 
 
 
+
 class Ball(Turtle):
     def __init__(self, starting_position_x, starting_position_y):
             super().__init__()
             self.color('white')
             self.penup()
             self.shape('circle')
-            self.goto(starting_position_x, starting_position_y)
-
+            self.x_move = 10
+            self.y_move = 10
+            self.ball_speed = 0.1
 
 
     def move(self):
-        self.goto(300, 140)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
 
-    def move_down(self):
-        new_y = self.ycor() -20
-        self.goto(self.xcor(), new_y)
+
+    def bounce(self):
+        self.y_move *= -1
+
+    def deflect(self):
+         self.x_move *= -1
+         self.adjust_ball_speed
+
+
+    def reset(self):
+         self.ball_speed = 0.1
+         self.goto(0,0)
+         self.deflect()
+
+
+    def adjust_ball_speed(self):
+        if self.ball_speed == 0.001:
+            pass
+        else:
+            self.ball_speed *= 0.7
 
--- Pong/main.py
+++ Pong/main.py
@@ -1,32 +1,56 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+import time
 
-
+WALLS = (270,-270)
 
 screen = Screen()
-left_paddle = Paddle(350, 0)
-right_paddle = Paddle(-350, 0)
+right_paddle = Paddle(350, 0)
+left_paddle = Paddle(-350, 0)
 ball = Ball(0, 0)
+scoreboard = Scoreboard()
 
 game_ON = True
 
-screen.bgcolor('black')
 screen.setup(width=800, height=600)
+screen.bgcolor('black')
 screen.title('PONG')
 screen.tracer(False)
 screen.listen()
 
-screen.onkey(left_paddle.move_up, 'Up')
-screen.onkey(left_paddle.move_down, 'Down')
 
-screen.onkey(right_paddle.move_up, 'w')
-screen.onkey(right_paddle.move_down, 's')
+
+screen.onkey(left_paddle.move_up, 'w')
+screen.onkey(left_paddle.move_down, 's')
+
+screen.onkey(right_paddle.move_up, 'Up')
+screen.onkey(right_paddle.move_down, 'Down')
 
 
 while game_ON:
-    ball.move()
-    screen.update()
 
+    screen.update()
+    ball.move()
+    time.sleep(ball.ball_speed)
+
+
+    if ball.ycor() >280 or ball.ycor() < -280:
+        ball.bounce()
+
+
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.deflect()
+
+    # Right Paddle
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.increase_Lscore()
+
+    # Left Paddle
+    elif ball.xcor() < -380:
+        ball.reset()
+        scoreboard.increase_Rscore()
 
 screen.exitonclick()
