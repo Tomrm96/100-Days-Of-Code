198c428d81d0e5b854dae8b94fa4b0dd38fb57e7
Tomrm96 <tomrutter96@hotmail.com>
2024-10-13 14:16:04 +0100

    Updated

--- Working_With_Files/main.py
+++ Working_With_Files/main.py
@@ -1,13 +1,25 @@
 # Overwrite
-with open('Day_24/my_file.txt', mode='w') as file:
+with open('./my_file.txt', mode='w') as file:
     file.write('Before')
 # Read
-with open("Day_24/my_file.txt", mode='r') as file:
+with open("./my_file.txt", mode='r') as file:
     contents = file.read()
     print(contents)
 # Append
-with open("Day_24/my_file.txt", mode='a') as file:
+with open("./my_file.txt", mode='a') as file:
     file.write('\nAfter')
 # Create a new File
-with open('Day_24/New_file.txt', mode='w') as file:
+with open('./New_file.txt', mode='w') as file:
     file.write('New')
+# Getting a File from somewhere else using the '/' Root & working Directory
+with open("/Users/USER/OneDrive/Desktop/New Text Document.txt", mode='r') as file:
+    contents = file.read()
+    print(contents)
+# Getting a File from somewhere else with absolute directory
+with open("../../Desktop/New Text Document.txt", mode='r') as file:
+    contents = file.read()
+    print(contents)
+
+    # Absolute  File Paths are always relative to the root folder C:\
+
+    # Relative File Paths are always relative to where you are currently working. 
